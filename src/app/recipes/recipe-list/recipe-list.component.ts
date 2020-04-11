9f10926bbb12133da03c9ab53886ac67f6268ab5
rleon2 <raulleon19@yahoo.com>
2020-04-11 12:33:35 -0700

    modifying components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
